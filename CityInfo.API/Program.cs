6cbd73f616cd0051d45557cb992765523011dfb2
jackson <s97403002a@gmail.com>
2024-04-18 19:05:32 +0800

    add 统一处理异常，为不同的异常设置不同的信息

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -8,13 +8,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region 统一处理异常，为不同的异常设置不同的信息
+
+builder.Services.AddProblemDetails(options => options.CustomizeProblemDetails = ctx =>
+{
+    ctx.ProblemDetails.Extensions.Add("additionalMetadata", "customValue");
+    ctx.ProblemDetails.Extensions.Add("MachineName", Environment.MachineName);
+});
+
+#endregion
+
 var app = builder.Build();
+
 //
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-     app.UseSwagger();
-     app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();

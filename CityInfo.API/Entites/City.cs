ad3b512a6e57252b6645cf426fdf0fcae82f8139
jackson <s97403002a@gmail.com>
2024-04-29 22:11:31 +0800

    Create Entity class

---
+++ CityInfo.API/Entites/City.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entites;
+
+public class City
+{
+    [Key]
+    [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
+    public ICollection<PointOfInterest> PointOfInterestDtos { get; set; } = new List<PointOfInterest>();
+
+    public City(string name)
+    {
+        Name = name;
+    }
+}
---
+++ CityInfo.API/Entites/PointOfInterest.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entites;
+
+public class PointOfInterest
+{
+    [Key]
+    [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    public City? City { get; set; }
+
+    [ForeignKey("CityId")]
+    public int CityId { get; set; }
+
+    public PointOfInterest(string name)
+    {
+        Name = name;
+    }
+}

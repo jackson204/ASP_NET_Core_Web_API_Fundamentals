da4fc5bc44f4e203792d463fc19c5c3322028d70
jackson <s97403002a@gmail.com>
2024-05-01 18:27:55 +0800

    dotnet ef migrations add dbset  --project .\CityInfo.API\ and dotnet ef database update -p .\CityInfo.API\

--- CityInfo.API/DbContexts/CityInfoContext.cs
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -19,4 +19,57 @@ public class CityInfoContext : DbContext
     //     optionsBuilder.UseSqlite("connectionstring");
     //     base.OnConfiguring(optionsBuilder);
     // }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>().HasData(
+            new City("New York City")
+      
+            {
+                Id = 1,
+                Description = "The one with that big park."
+            },
+            new City("Antwerp")
+          
+            {
+                Id = 2,
+                Description = "The one with the cathedral that was never really finished."
+            },
+            new City("Paris")
+            {
+                Id = 3,
+                Description = "The one with that big tower."
+            });
+
+        modelBuilder.Entity<PointOfInterest>().HasData(
+            new PointOfInterest("Central Park")
+            {
+                Id = 1,
+                CityId = 1
+            },
+            new PointOfInterest("Empire State Building")
+            {
+                Id = 2,
+                CityId = 1
+            },
+            new PointOfInterest("Cathedral of Our Lady")
+            {
+                Id = 3,
+                CityId = 2
+            },
+            new PointOfInterest("Antwerp Central Station")
+            {
+                Id = 4,
+                CityId = 2
+            },
+            new PointOfInterest("Eiffel Tower")
+            {
+                Id = 5,
+                CityId = 3
+            });
+        
+        
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- CityInfo.API/Entites/PointOfInterest.cs
+++ CityInfo.API/Entites/PointOfInterest.cs
@@ -13,6 +13,9 @@ public class PointOfInterest
     [MaxLength(50)]
     public string Name { get; set; }
 
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
     public City? City { get; set; }
 
     [ForeignKey("CityId")]

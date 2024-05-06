3bd2399e156447e38d853aa902e4dfdcec23487a
jackson <s97403002a@gmail.com>
2024-05-06 22:33:32 +0800

    adjust Controllers use ICityInfoRepository

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers;
@@ -7,25 +8,34 @@ namespace CityInfo.API.Controllers;
 [Route("api/[controller]")]
 public class CitiesController : ControllerBase
 {
-    private readonly  CitiesDataStore _citiesDataStore;
-    public CitiesController(CitiesDataStore citiesDataStore)
+    private ICityInfoRepository _cityInfoRepository;
+
+    public CitiesController(ICityInfoRepository cityInfoRepository)
     {
-        _citiesDataStore = citiesDataStore;
+        _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
     }
+
     [HttpGet]
-    public IActionResult GetCities()
+    public async Task<IActionResult> GetCities()
     {
-        return Ok(_citiesDataStore.Cities);
+        var cities = await _cityInfoRepository.GetCitiesAsync();
+        return Ok(cities.Select(c => new CityWithoutPointsOfInterestDto()
+        {
+            Id = c.Id,
+            Name = c.Name,
+            Description = c.Description
+        }).ToList());
     }
 
     [HttpGet("{id}")]
     public IActionResult GetCity(int id)
     {
-        var cityDto = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
-        if (cityDto == null)
-        {
-            return NotFound();
-        }
-        return Ok(cityDto);
+        // var cityDto = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+        // if (cityDto == null)
+        // {
+        //     return NotFound();
+        // }
+        // return Ok(cityDto);
+        return Ok();
     }
 }
---
+++ CityInfo.API/Models/CityWithoutPointsOfInterestDto.cs
@@ -0,0 +1,10 @@
+namespace CityInfo.API.Models;
+
+public class CityWithoutPointsOfInterestDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string? Description { get; set; }
+}
